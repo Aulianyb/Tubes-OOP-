add0eae3ac80801eb8ad840c62f654508df51426
graciatb <graciatbarus@gmail.com>
2023-04-19 16:52:08 +0700

    add ruangan, rumah, and world

---
+++ app/src/main/java/simplicity/Ruangan.java
@@ -0,0 +1,34 @@
+package simplicity;
+import java.util.*;
+
+public class Ruangan {
+    private String namaRuangan;
+    private int panjang = 6;
+    private int lebar = 6;
+    // private List<Furnitur> objekList = new ArrayList<Furnitur>();
+    // private HashMap<Point, Furnitur> objekLoc = new ArrayList<Point<Integer, Integer>();
+
+    public Ruangan(String namaRuangan) {
+        this.namaRuangan = namaRuangan;
+    }
+
+    public String getNama() {
+        return this.namaRuangan;
+    }
+
+    public int getPanjang() {
+        return this.panjang;
+    }
+
+    public int getLebar() {
+        return this.lebar;
+    }
+
+    public int getLuas() {
+        return(this.panjang*this.lebar);
+    }
+
+    // public void addObjek(Furnitur furnitur, Point location) {}
+
+    // public void removeObjek(Furnitur furnitur) {}
+}
---
+++ app/src/main/java/simplicity/World.java
@@ -0,0 +1,39 @@
+package simplicity;
+import java.util.*;
+
+public class World {
+    private int panjang;
+    private int lebar;
+    private HashMap<Point, Rumah> rumahList = new HashMap<Point, Rumah>();
+    private Waktu waktu;
+
+    public World(int panjang, int lebar, Waktu waktu) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.waktu = waktu;
+    }
+
+    public int getPanjang() {
+        return this.panjang;
+    }
+
+    public int getLebar() {
+        return this.lebar;
+    }
+
+    public int getLuas() {
+        return(this.panjang*this.lebar);
+    }
+
+    public HashMap<Point, Rumah> getRumahList() {
+        return this.rumahList;
+    }
+
+    public Waktu getWaktu() {
+        return this.waktu;
+    }
+
+    public void addRumah(Point location, Rumah rumah) {
+        rumahList.put(location, rumah);
+    }
+}
