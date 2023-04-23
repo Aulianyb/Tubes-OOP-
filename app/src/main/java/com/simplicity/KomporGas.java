bb531d8e1aff2b9b05d60ceb75b52681460a6f0e
FAlgani <18221108@std.stei.itb.ac.id>
2023-04-23 22:49:44 +0700

    added KomporGas

---
+++ app/src/main/java/com/simplicity/KomporGas.java
@@ -0,0 +1,10 @@
+package com.simplicity;
+
+public class KomporGas extends Kompor {
+    public KomporGas(String namaObjek) {
+        super(namaObjek);
+        setPanjang(2);
+        setLebar(1);
+        setHarga(100);
+    }
+}
