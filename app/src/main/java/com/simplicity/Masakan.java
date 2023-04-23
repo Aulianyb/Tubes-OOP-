35b74ed06ac0a5a638811434890f90ca86ed8319
FAlgani <18221108@std.stei.itb.ac.id>
2023-04-23 22:45:54 +0700

    added Masakan

---
+++ app/src/main/java/com/simplicity/Masakan.java
@@ -0,0 +1,43 @@
+package com.simplicity;
+import java.util.*;
+
+public class Masakan extends Makanan {
+    private List<BahanMakanan> ingredients = new ArrayList<BahanMakanan>();
+
+    //Konstruktor
+    public Masakan(String namaObjek) {
+        super(namaObjek);
+
+        if (namaObjek.equals("Nasi Ayam")) {
+            setNilaiKekenyangan(16);
+            ingredients.add(new BahanMakanan("Nasi"));
+            ingredients.add(new BahanMakanan("Ayam"));
+        }
+        else if (namaObjek.equals("Nasi Kari")) {
+            setNilaiKekenyangan(30);
+            ingredients.add(new BahanMakanan("Nasi"));
+            ingredients.add(new BahanMakanan("Kentang"));
+            ingredients.add(new BahanMakanan("Wortel"));
+            ingredients.add(new BahanMakanan("Sapi"));
+        }
+        else if (namaObjek.equals("Susu Kacang")) {
+            setNilaiKekenyangan(5);
+            ingredients.add(new BahanMakanan("Susu"));
+            ingredients.add(new BahanMakanan("Kacang"));
+        }
+        else if (namaObjek.equals("Tumis Sayur")) {
+            setNilaiKekenyangan(5);
+            ingredients.add(new BahanMakanan("Wortel"));
+            ingredients.add(new BahanMakanan("Bayam"));
+        }
+        else if (namaObjek.equals("Bistik")) {
+            setNilaiKekenyangan(22);
+            ingredients.add(new BahanMakanan("Kentang"));
+            ingredients.add(new BahanMakanan("Sapi"));
+        }
+    }
+
+    public List<BahanMakanan> getBahan() {
+        return ingredients;
+    }
+}
