6bb89865c0f7aff8bbbf6ef12960d6b907e2e41a
FAlgani <18221108@std.stei.itb.ac.id>
2023-04-23 22:44:16 +0700

    added KasurKingSize

---
+++ app/src/main/java/com/simplicity/KasurKingSize.java
@@ -0,0 +1,10 @@
+package com.simplicity;
+
+public class KasurKingSize extends Kasur {
+    public KasurKingSize(String namaObjek) {
+        super(namaObjek);
+        setPanjang(5);
+        setLebar(2);
+        setHarga(150);
+    }
+}
