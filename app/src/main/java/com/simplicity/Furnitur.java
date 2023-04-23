edbffd1d20a4fcb86365a801ce4d1dbd9c52a39d
FAlgani <18221108@std.stei.itb.ac.id>
2023-04-23 22:43:12 +0700

    added Furnitur

---
+++ app/src/main/java/com/simplicity/Furnitur.java
@@ -0,0 +1,48 @@
+package com.simplicity;
+
+public class Furnitur extends ObjekGame implements BisaDibeli{
+    private int panjang;
+    private int lebar;
+    private Status validAction;
+    private int harga;
+
+    public Furnitur(String namaObjek) {
+        super(namaObjek);
+        this.panjang = 0;
+        this.lebar = 0;
+    }
+
+    //Getter
+    public int getPanjang() {
+        return panjang;
+    }
+
+    public int getLebar() {
+        return lebar;
+    }
+
+    public Status getValidAction() {
+        return validAction;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    //Setter
+    public void setPanjang(int panjang) {
+        this.panjang = panjang;
+    }
+
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
+    }
+
+    public void setValidAction(Status validAction) {
+        this.validAction = validAction;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
