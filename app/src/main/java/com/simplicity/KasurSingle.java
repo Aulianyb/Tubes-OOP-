3707aeaa9eb68952010cd0a7ab2b02821b800726
FAlgani <18221108@std.stei.itb.ac.id>
2023-04-23 22:43:59 +0700

    added KasurSingle

---
+++ app/src/main/java/com/simplicity/KasurSingle.java
@@ -0,0 +1,11 @@
+package com.simplicity;
+
+public class KasurSingle extends Kasur {
+    //Konstruktor
+    public KasurSingle(String namaObjek) {
+        super(namaObjek);
+        setPanjang(4);
+        setLebar(1);
+        setHarga(50);
+    }
+}
