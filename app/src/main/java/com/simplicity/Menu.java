dc23ea7688da0cfa1d827bac53aff6c3cfc281e7
reinhartlim1 <18221154@std.stei.itb.ac.id>
2023-05-06 00:00:00 +0700

    remove import

--- app/src/main/java/com/simplicity/Menu.java
+++ app/src/main/java/com/simplicity/Menu.java
@@ -1,10 +1,6 @@
 package com.simplicity;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
-// import com.google.gson.Gson;
+
 public class Menu {
 
     public Menu() {
